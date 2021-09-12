f531c02f13d9e71bbfa6d8f56294cbb4fcc475ac
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-09-12 14:41:53 -0400

    Update index.js
    update to add top 10 movies

--- index.js
+++ index.js
@@ -1,2 +1,66 @@
 const express = require('express');
 const app = express();
+
+let movies = [
+    {
+      title: 'movie name',
+      director: 'name'
+    },
+    {
+      title: 'movie name',
+      director: 'name'
+    },
+    {
+      title: 'movie name',
+      director: 'name'
+    },
+    {
+      title: 'movie name',
+      director: 'name'
+    },
+    {
+      title: 'movie name',
+      director: 'name'
+    },
+    {
+      title: 'movie name',
+      director: 'name'
+    },
+    {
+      title: 'movie name',
+      director: 'name'
+    },
+    {
+      title: 'movie name',
+      director: 'name'
+    },
+    {
+      title: 'movie name',
+      director: 'name'
+    },
+    {
+      title: 'movie name',
+      director: 'name'
+    },
+  ];
+  
+  // GET requests
+  app.get('/', (req, res) => {
+    res.send('Welcome!');
+  });
+  
+  app.get('/documentation', (req, res) => {                  
+    res.sendFile('public/documentation.html', { root: __dirname });
+  });
+  
+  app.get('/movies', (req, res) => {
+    res.json(topBooks);
+  });
+
+  app.use('/movies', express.static('public'));
+  
+  
+  // listen for requests
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
