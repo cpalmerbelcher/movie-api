08d6224e97168bb6df189f8319f9eab920e7512f
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-10-06 12:34:37 -0400

    Update index.js
    update passport for postman authentication

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
 });
 
   //get list of all movies
-  app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+  app.get('/movies', passport.authenticate('jwt', { session: false }),(req, res) => {
     Movies.find()
       .then((movies) => {
         res.status(201).json(movies);
