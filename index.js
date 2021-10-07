8bc72bdd9e0080ebfcbc25038b4c1f8237e3f80d
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-10-07 18:58:38 -0400

    Update index.js
    update connection URI

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const Directors = Models.Directors;
 
 //conntecting database with connection URI
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', 
-mongoose.connect( process.env.CONNECTION_URL, 
+mongoose.connect( process.env.CONNECTION_URI, 
   { useNewUrlParser: true, useUnifiedTopology: true });
    
 //activating body-parser
