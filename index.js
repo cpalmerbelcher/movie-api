71757415b5c192266731fe440939f9f83df81c20
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-10-19 15:32:34 -0400

    Update index.js
    updating the movie endpoint to give my React application to the API

--- index.js
+++ index.js
@@ -48,13 +48,13 @@ app.get('/', (req, res) => {
 });
 
   //get list of all movies
-  app.get('/movies', passport.authenticate('jwt', { session: false }),(req, res) => {
+  app.get('/movies', function (req, res) {
     Movies.find()
-      .then((movies) => {
+      .then(function (movies) {
         res.status(201).json(movies);
       })
-      .catch((err) => {
-        console.error(err);
+      .catch(function (error) {
+        console.error(error);
         res.status(500).send('Error: ' + err);
       });
   });
